755f2e879389cdf7ee73b1ef26d97a1c70998485
hadoop <hadoop@roomas-MacBook-Pro.local>
2018-06-10 22:48:30 -0400

    first commit

---
+++ app.js
@@ -0,0 +1,6 @@
+var app= angular.module("firstApp", []);
+
+app.controller('myCtrl', function($scope){
+	$scope.first ="kaka";
+	$scope.names ={name: "kaka", firstname: "Hk", lastname:"singh"};
+});
